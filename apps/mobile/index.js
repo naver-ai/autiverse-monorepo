5103654ad3883999b11737e680f0ec68bc851c56
Young-Ho Kim <yghokim@younghokim.net>
2025-07-25 23:55:46 +0900

    Support exclusive hostname in dev mode.

--- apps/mobile/index.js
+++ apps/mobile/index.js
@@ -12,7 +12,7 @@ console.log("Starting mobile app in debug mode:", __DEV__);
 SplashScreen.preventAutoHideAsync();
 
 const protocol = process.env.EXPO_PUBLIC_USE_HTTPS === '1' && (!__DEV__ || process.env.EXPO_PUBLIC_USE_HTTPS_IN_DEV === '1') ? 'https' : 'http';
-const backendUrl = __DEV__ ? (isAndroidEmulator() ? `${protocol}://10.0.2.2:3000` : `${protocol}://localhost:3000`) : `${protocol}://${process.env.EXPO_PUBLIC_BACKEND_HOSTNAME}:${process.env.EXPO_PUBLIC_BACKEND_PORT}`;
+const backendUrl = __DEV__ ? (isAndroidEmulator() ? `${protocol}://10.0.2.2:3000` : `${protocol}://${process.env.EXPO_PUBLIC_BACKEND_HOSTNAME_DEV || 'localhost'}:3000`) : `${protocol}://${process.env.EXPO_PUBLIC_BACKEND_HOSTNAME}:${process.env.EXPO_PUBLIC_BACKEND_PORT}`;
 
 NetworkHelper.init(
     backendUrl,
@@ -21,7 +21,7 @@ NetworkHelper.init(
 
 SocketManager.init(
     process.env.EXPO_PUBLIC_USE_HTTPS == "1" ? "wss" : "ws",
-    __DEV__ ? (isAndroidEmulator() ? '10.0.2.2' : 'localhost') : process.env.EXPO_PUBLIC_BACKEND_HOSTNAME,
+    __DEV__ ? (isAndroidEmulator() ? '10.0.2.2' : (process.env.EXPO_PUBLIC_BACKEND_HOSTNAME_DEV || 'localhost')) : process.env.EXPO_PUBLIC_BACKEND_HOSTNAME,
     __DEV__ ? 3000 : process.env.EXPO_PUBLIC_BACKEND_PORT,
   "dyad",
   false
