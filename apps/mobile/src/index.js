3a2b2d148007e6bbd5d906dc54d3737b082af9c4
yangmigyeong <ytr1133@naver.com>
2025-09-17 13:52:51 +0900

    Update index.js

--- apps/mobile/src/index.js
+++ apps/mobile/src/index.js
@@ -12,8 +12,8 @@ console.log("Starting mobile app in debug mode:", __DEV__);
 SplashScreen.preventAutoHideAsync();
 
 const protocol = process.env.EXPO_PUBLIC_USE_HTTPS === '1' && (!__DEV__ || process.env.EXPO_PUBLIC_USE_HTTPS_IN_DEV === '1') ? 'https' : 'http';
-// const backendUrl = __DEV__ ? (isAndroidEmulator() ? `${protocol}://10.0.2.2:3000` : `${protocol}://${process.env.EXPO_PUBLIC_BACKEND_HOSTNAME_DEV || 'localhost'}:3000`) : `${protocol}://${process.env.EXPO_PUBLIC_BACKEND_HOSTNAME}:${process.env.EXPO_PUBLIC_BACKEND_PORT}`;
-const backendUrl = __DEV__ ? `${protocol}://10.66.106.30:3000` : `${protocol}://${process.env.EXPO_PUBLIC_BACKEND_HOSTNAME}:${process.env.EXPO_PUBLIC_BACKEND_PORT}`;
+const backendUrl = __DEV__ ? (isAndroidEmulator() ? `${protocol}://10.0.2.2:3000` : `${protocol}://${process.env.EXPO_PUBLIC_BACKEND_HOSTNAME_DEV || 'localhost'}:3000`) : `${protocol}://${process.env.EXPO_PUBLIC_BACKEND_HOSTNAME}:${process.env.EXPO_PUBLIC_BACKEND_PORT}`;
+// const backendUrl = __DEV__ ? `${protocol}://10.66.106.30:3000` : `${protocol}://${process.env.EXPO_PUBLIC_BACKEND_HOSTNAME}:${process.env.EXPO_PUBLIC_BACKEND_PORT}`;
 
 NetworkHelper.init(
     backendUrl,
@@ -22,8 +22,8 @@ NetworkHelper.init(
 
 SocketManager.init(
     protocol == 'https' ? "wss" : "ws",
-    // __DEV__ ? (isAndroidEmulator() ? '10.0.2.2' : (process.env.EXPO_PUBLIC_BACKEND_HOSTNAME_DEV || 'localhost')) : process.env.EXPO_PUBLIC_BACKEND_HOSTNAME,
-    __DEV__ ? '10.66.106.30' : process.env.EXPO_PUBLIC_BACKEND_HOSTNAME,
+    __DEV__ ? (isAndroidEmulator() ? '10.0.2.2' : (process.env.EXPO_PUBLIC_BACKEND_HOSTNAME_DEV || 'localhost')) : process.env.EXPO_PUBLIC_BACKEND_HOSTNAME,
+    // __DEV__ ? '10.66.106.30' : process.env.EXPO_PUBLIC_BACKEND_HOSTNAME,
     __DEV__ ? 3000 : process.env.EXPO_PUBLIC_BACKEND_PORT,
   "dyad",
   false
